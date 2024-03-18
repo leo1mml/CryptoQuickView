3060d40b852ad1d711f63313b310dd7ce41ad49b
Leonel Lima <mml_leo@hotmail.com>
2024-03-18 15:38:36 +0000

    Add initial project

---
+++ CryptoQuickView/CryptoQuickViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoQuickViewApp.swift
+//  CryptoQuickView
+//
+//  Created by Leonel Lima on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoQuickViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
