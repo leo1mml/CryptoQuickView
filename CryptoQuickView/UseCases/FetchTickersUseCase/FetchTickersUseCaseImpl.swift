3d0c19f4b1b3da620ccbc32dff474d48c327662f
Leonel Lima <mml_leo@hotmail.com>
2024-03-20 12:26:26 +0000

    Add optional init

--- CryptoQuickView/UseCases/FetchTickersUseCase/FetchTickersUseCaseImpl.swift
+++ CryptoQuickView/UseCases/FetchTickersUseCase/FetchTickersUseCaseImpl.swift
@@ -23,29 +23,7 @@ class FetchTickersUseCaseImpl: FetchTickersUseCase {
                     if let jsonArray = jsonArray {
                         var tradeDataArray: [TradeData] = []
                         for array in jsonArray {
-                            if array.count == 11, let symbol = array[0] as? String {
-                                let bid = array[1] as? Double ?? 0.0
-                                let bidSize = array[2] as? Double ?? 0.0
-                                let ask = array[3] as? Double ?? 0.0
-                                let askSize = array[4] as? Double ?? 0.0
-                                let dailyChange = array[5] as? Double ?? 0.0
-                                let dailyChangePercentage = array[6] as? Double ?? 0.0
-                                let lastPrice = array[7] as? Double ?? 0.0
-                                let volume = array[8] as? Double ?? 0.0
-                                let high = array[9] as? Double ?? 0.0
-                                let low = array[10] as? Double ?? 0.0
-                                
-                                let tradeData = TradeData(symbol: symbol,
-                                                          bid: bid,
-                                                          bidSize: bidSize,
-                                                          ask: ask,
-                                                          askSize: askSize,
-                                                          dailyChange: dailyChange,
-                                                          dailyChangePercentage: dailyChangePercentage,
-                                                          lastPrice: lastPrice,
-                                                          volume: volume,
-                                                          high: high,
-                                                          low: low)
+                            if let tradeData = TradeData(dataArray: array) {
                                 tradeDataArray.append(tradeData)
                             }
                         }
--- CryptoQuickView/UseCases/FetchTickersUseCase/Model/TradeData.swift
+++ CryptoQuickView/UseCases/FetchTickersUseCase/Model/TradeData.swift
@@ -14,4 +14,60 @@ struct TradeData {
     let volume: Double
     let high: Double
     let low: Double
+    
+    init(
+        symbol: String,
+        bid: Double,
+        bidSize: Double,
+        ask: Double,
+        askSize: Double,
+        dailyChange: Double,
+        dailyChangePercentage: Double,
+        lastPrice: Double,
+        volume: Double,
+        high: Double,
+        low: Double
+    ) {
+        self.symbol = symbol
+        self.bid = bid
+        self.bidSize = bidSize
+        self.ask = ask
+        self.askSize = askSize
+        self.dailyChange = dailyChange
+        self.dailyChangePercentage = dailyChangePercentage
+        self.lastPrice = lastPrice
+        self.volume = volume
+        self.high = high
+        self.low = low
+    }
+    
+    init?(dataArray: [Any]) {
+        if dataArray.count == 11, let symbol = dataArray[0] as? String {
+            let bid = dataArray[1] as? Double ?? 0.0
+            let bidSize = dataArray[2] as? Double ?? 0.0
+            let ask = dataArray[3] as? Double ?? 0.0
+            let askSize = dataArray[4] as? Double ?? 0.0
+            let dailyChange = dataArray[5] as? Double ?? 0.0
+            let dailyChangePercentage = dataArray[6] as? Double ?? 0.0
+            let lastPrice = dataArray[7] as? Double ?? 0.0
+            let volume = dataArray[8] as? Double ?? 0.0
+            let high = dataArray[9] as? Double ?? 0.0
+            let low = dataArray[10] as? Double ?? 0.0
+            
+            self.symbol = symbol
+            self.bid = bid
+            self.bidSize = bidSize
+            self.ask = ask
+            self.askSize = askSize
+            self.dailyChange = dailyChange
+            self.dailyChangePercentage = dailyChangePercentage
+            self.lastPrice = lastPrice
+            self.volume = volume
+            self.high = high
+            self.low = low
+        } else {
+            return nil
+        }
+    }
+    
 }
